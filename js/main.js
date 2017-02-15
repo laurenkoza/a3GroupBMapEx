8d8c967a6835222dfc61331dbf74e9596f28942a
Koza <l_koza3@tss-hr420-dm01-2.local>
2017-02-15 11:49:31 -0500

    Initial file dump

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function(){
+
+	var map, marker;
+
+
+	function iniMap() {
+		map = new google.maps.Map(document.querySelector('#map-wrapper'),
+			{
+				center: {lat: 42.983233, lng: -81.250688},
+				zoom: 14 // have to test in firefox
+			}// options object
+		); //what element you want to draw ur map in
+
+		marker= new google.maps.Marker({
+			position: { lat: 42.983233, lng: -81.250688 },
+			map: map,
+			title: "Paddy's secret hut"
+		});
+	}
+
+
+
+
+
+
+})();
